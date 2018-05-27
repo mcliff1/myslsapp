7ba45c11df7bbf78887d6746f2863e0c10d4048f
Matt Cliff <matt@denvercliffs.com>
2018-05-27 14:28:11 -0600

    flushed out deploy script

--- tms/deploy-web.sh
+++ tms/deploy-web.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+# this would come from AWS Paramter Store  /tmsbase/webHostName
+#  (or cloudformation output  tms-)
+#  make AWS call to get it...
+TARGET_BUCKET=s3://tmsbase-webbucket-0okwetxp1389
+# source variables
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+
+cd $__dir
+npm install
+npm run build
+
+aws s3 sync build ${TARGET_BUCKET}
