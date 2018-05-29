1356c83f7b9ab81c1ee0ca08858eb5ec1f3cb0c2
Matt Cliff <matt@denvercliffs.com>
2018-05-29 18:29:37 +0000

    updates to streamline

--- tms/deploy-web.sh
+++ tms/deploy-web.sh
@@ -3,7 +3,11 @@
 # this would come from AWS Paramter Store  /tmsbase/webHostName
 #  (or cloudformation output  tms-)
 #  make AWS call to get it...
-TARGET_BUCKET=s3://tmsbase-webbucket-9okwetxp1389
+REGION=us-west-2
+BUCKET_NAME=`aws ssm get-parameter --name "/tmsbase/webBucketName" --region ${REGION} | jq -r .Parameter.Value`
+
+TARGET_BUCKET=s3://${BUCKET_NAME}
+
 # source variables
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
