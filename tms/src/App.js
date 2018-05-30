256a844ee5f288b99c79e0059b13ced8e76081c1
Matt Cliff <matt@denvercliffs.com>
2018-05-30 12:23:14 -0600

    have basic cognito sign-in page

--- tms/src/App.js
+++ tms/src/App.js
@@ -1,30 +1,81 @@
-import React from 'react';
-import { BrowserRouter as Router, Route  } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom';
+import { Auth } from 'aws-amplify';
+
 import './App.css';
 import Home from './Home.js';
 import Customer from './Customer.js';
 import Load from './Load.js';
 import Carrier from './Carrier.js';
 import AppNav from './AppNav.js';
-import MyTodo from './todo/MyTodo.js'
+import MyTodo from './todo/MyTodo.js';
+import Login from './containers/Login.js';
+import NotFound from './containers/NotFound.js';
+import AuthenticatedRoute from './components/AuthenticatedRoute.js';
 
 
 // https://react-bootstrap.github.io/components/label/
+// https://github.com/AnomalyInnovations/serverless-stack-demo-client/
 
-//class App extends Component {
-const App = () => (
+class App extends Component {
+//const App = () => (
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthenticated: false,
+      isAuthenticating: true
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      if (await Auth.currentSession()) {
+        this.userHasAuthenticated(true);
+      }
+    }
+    catch (e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+    this.setState({ isAuthenticating: false });
+  }
+
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
+
+  handleLogout = async event => {
+    await Auth.signOut();
+    this.userHasAuthenticated(false);
+    //this.props.history.push("/login");
+  }
+
+  render() {
+    const childProps = {
+      isAuthenitcated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenitcated
+    };
+
+
+    return(
   <Router>
     <div className="App">
       <AppNav />
-       <div className="container">
-        <Route exact path="/" component={Home} />
-        <Route path="/customer" component={Customer} />
-        <Route path="/load" component={Load} />
-        <Route path="/mytodo" component={MyTodo} />
-        <Route path="/carrier" component={Carrier} />
-       </div>
+      <Switch>
+        <Route exact path="/" component={Home} props={childProps} />
+        <Route exact path="/login" component={Login} props={childProps}/>
+        <Route exact path="/customer" component={Customer} props={childProps}/>
+        <AuthenticatedRoute exact path="/load" component={Load} props={childProps}/>
+        <Route exact path="/mytodo" component={MyTodo} props={childProps}/>
+        <Route exact path="/carrier" component={Carrier} props={childProps}/>
+        { /* catch any unmatched route */ }
+        <Route component={NotFound} />
+       </Switch>
     </div>
   </Router>
-);
+    )
+  };
+}
 
 export default App;
--- tms/src/todo/reducers/index.js
+++ tms/src/todo/reducers/index.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import todos from './todos.js';
 import visibilityFilter from './visibilityFilter.js';
 
-
 export default combineReducers({
     todos,
     visibilityFilter
